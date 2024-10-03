4f14dc05d79086669af133ef1aad88d7e8f50519
reeseque <reese.roque@ciit.edu.ph>
2024-10-03 11:15:20 +0800

    yipee

---
+++ src/components/gallery.jsx
@@ -0,0 +1,35 @@
+import { Image } from "./image";
+import React from "react";
+
+export const Gallery = (props) => {
+  return (
+    <div id="portfolio" className="text-center">
+      <div className="container">
+        <div className="section-title">
+          <h2>Dine</h2>
+          <p>
+          Savor alien-inspired dishes at Tequila Moonrise, where every plate is a work of art. Enjoy breathtaking space-themed decor and cosmic views as you dine. Our unique atmosphere and flavors make each meal an unforgettable journey.
+          </p>
+        </div>
+        <div className="row">
+          <div className="portfolio-items">
+            {props.data
+              ? props.data.map((d, i) => (
+                  <div
+                    key={`${d.title}-${i}`}
+                    className="col-sm-6 col-md-4 col-lg-4"
+                  >
+                    <Image
+                      title={d.title}
+                      largeImage={d.largeImage}
+                      smallImage={d.smallImage}
+                    />
+                  </div>
+                ))
+              : "Loading..."}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/services.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export const Services = (props) => {
+  return (
+    <div id="services" className="text-center">
+      <div className="container">
+        <div className="section-title">
+          <h2>Our Services</h2>
+          <p>
+          At Tequila Moonrise, we offer a range of luxurious services designed to make your stay truly out of this world. From our alien-inspired dining experience to our stellar accommodations, every service is crafted to immerse you in the wonders of the cosmos. Whether you're here to relax in our cosmic spa or enjoy a stargazing night, we ensure that your time with us is as extraordinary as the universe itself.
+          </p>
+        </div>
+        <div className="row">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className="col-md-4">
+                  {" "}
+                  <i className={d.icon}></i>
+                  <div className="service-desc">
+                    <h3>{d.name}</h3>
+                    <p>{d.text}</p>
+                  </div>
+                </div>
+              ))
+            : "loading"}
+        </div>
+      </div>
+    </div>
+  );
+};
